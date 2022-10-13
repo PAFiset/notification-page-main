46ec9b1ad2047234ac632eb4d7f10e3dc06f1650
PAFiset <jmpaof45@gmail.com>
2022-10-13 16:47:56 -0400

    set homepage static images and data

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,113 @@
 import styles from '../styles/Home.module.scss'
 import Image from 'next/image'
 import markwebberimage from "../media/avatar-mark-webber.png"
-import kimberlysmithimage from "../media/avatar-kimberly-smith.webp"
 import angelagrayimage from "../media/avatar-angela-gray.webp"
+import jacobthompsonimage from "../media/avatar-jacob-thompson.webp"
+import rizkyhasanuddinimage from "../media/avatar-rizky-hasanuddin.webp"
+import kimberlysmithimage from "../media/avatar-kimberly-smith.webp"
+import nathanpetersonimage from "../media/avatar-nathan-peterson.webp"
+import annakimimage from "../media/avatar-anna-kim.webp" 
+import { useState,useEffect } from 'react'
 
 export default function Home() {
+
+  const [newMessage,setNewMessageNum] = useState(0); 
+  const [messageArray,setMessageArray] = useState([true,true,true,false,false,false,false]);
+
+  
+
+  const clickHandler = () => {
+    var newMessageArray = messageArray;
+
+    newMessageArray.forEach((element,index) => {
+      newMessageArray[index] = false;
+    });
+
+    console.log(newMessageArray);
+
+    setMessageArray(newMessageArray);
+    
+  }
+
+
   return (
     <div>
 
       <main>
         <div className="flex_container" >
           <div className={styles.flex_content}>
-            <div className={styles.notification} >
+            <div className={styles.notification_display}>
+              <div className={styles.title} >
+                Notification
+              </div>
+              <div className={styles.notification_number} >
+                3
+              </div>
+              <div className={styles.mark_all_as_read} onClick={clickHandler} >
+                Mark all as read
+              </div>
+            </div>
+            <div className={ messageArray[0] ? styles.notification + " " + styles.unseen : styles.notification} >
               <div className={styles.profile_picture_frame}>
-                <Image className={styles.profile_picture}  src={markwebberimage} alt="Mark-webber" width="50px" height="50px"  />
+                <Image className={styles.profile_picture}  src={markwebberimage} alt="Mark webber" width="50px" height="50px"  />
               </div>
               <div>
                 <div><b>Mark Webber</b> reacted to your recent post <b>My first tournament today!</b></div>
                 <div className={styles.time} >1m ago</div>
               </div>
             </div>
-            <div className={styles.notification +" "+ styles.seen} >
+            <div className={ messageArray[1] ? styles.notification + " " + styles.unseen : styles.notification} >
+              <div className={styles.profile_picture_frame}>
+                <Image className={styles.profile_picture}  src={angelagrayimage} alt="angela gray" width="50px" height="50px"  />
+              </div>
+              <div>
+                <div><b>Angela Gray</b> followed you!</div>
+                <div className={styles.time} >5m ago</div>
+              </div>
+            </div>
+            <div className={ messageArray[2] ? styles.notification + " " + styles.unseen : styles.notification} >
+              <div className={styles.profile_picture_frame}>
+                <Image className={styles.profile_picture}  src={jacobthompsonimage} alt="jacob thompson" width="50px" height="50px"  />
+              </div>
+              <div>
+                <div><b>Jacob thompson</b> has joined your group <b>Chess Club</b></div>
+                <div className={styles.time} >1day ago</div>
+              </div>
+            </div>
+            <div className={ messageArray[3] ? styles.notification + " " + styles.unseen : styles.notification} >
+              <div className={styles.profile_picture_frame}>
+                <Image className={styles.profile_picture}  src={rizkyhasanuddinimage} alt="rizky hasanuddin" width="50px" height="50px"  />
+              </div>
+              <div>
+                <div><b>Rizky Hasanuddin</b> sent you a private message</div>
+                <div className={styles.time} >5days ago</div>
+              </div>
+            </div>
+            <div className={ messageArray[4] ? styles.notification + " " + styles.unseen : styles.notification} >
+              <div className={styles.profile_picture_frame}>
+                <Image className={styles.profile_picture}  src={kimberlysmithimage} alt="kimberly smith" width="50px" height="50px"  />
+              </div>
+              <div>
+                <div><b>Kimberly Smith</b> Commented on your picture</div>
+                <div className={styles.time} >1week ago</div>
+              </div>
+            </div>
+            <div className={ messageArray[5] ? styles.notification + " " + styles.unseen : styles.notification} >
+              <div className={styles.profile_picture_frame}>
+                <Image className={styles.profile_picture}  src={nathanpetersonimage} alt="nathan peterson" width="50px" height="50px"  />
+              </div>
+              <div>
+                <div><b>Nathan Peterson</b> reacted to your recent post <b>5 end-game strategies to increase your win rate</b></div>
+                <div className={styles.time} >2week ago</div>
+              </div>
+            </div>
+            <div className={ messageArray[6] ? styles.notification + " " + styles.unseen : styles.notification} >
               <div className={styles.profile_picture_frame}>
                 <Image className={styles.profile_picture}  src={angelagrayimage} alt="angela-gray" width="50px" height="50px"  />
               </div>
               <div>
-                <div>Angela Gray followed you!</div>
-                <div className={styles.time} >5m ago</div>
+                <div><b>Anna Kim</b> left the group <b>Chest Club</b></div>
+                <div className={styles.time} >2week ago</div>
               </div>
             </div>
           </div>
